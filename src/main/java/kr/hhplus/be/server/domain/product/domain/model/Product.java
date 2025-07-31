194847a337dd45f5ee3b9cd35a10f24202f91643
eunsol-an <dev.eunsol@gmail.com>
2025-08-01 06:39:43 +0900

    refactor: ProductMapper를 클래스 구현으로 전환 및 Product, ProductJpaEntity 관련 로직 개선
    - Product 생성자에 Builder 패턴 추가 및 팩토리 메서드 구현
    - ProductJpaEntity에 @AllArgsConstructor 추가
    - ProductMapper를 인터페이스에서 클래스로 변경하고 매핑 로직 내부 구현

--- src/main/java/kr/hhplus/be/server/domain/product/domain/model/Product.java
+++ src/main/java/kr/hhplus/be/server/domain/product/domain/model/Product.java
@@ -1,18 +1,43 @@
 package kr.hhplus.be.server.domain.product.domain.model;
 
 import kr.hhplus.be.server.exception.ApiException;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import static kr.hhplus.be.server.exception.ErrorCode.OUT_OF_STOCK;
 
 @Getter
-@AllArgsConstructor
 public class Product {
     private Long id;
     private String name;
     private Long price;
     private Integer stockQuantity;
+
+    @Builder
+    private Product(Long id, String name, Long price, Integer stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public static Product of(Long id, String name, Long price, Integer stockQuantity) {
+        return Product.builder()
+                .id(id)
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .build();
+    }
+
+    public static Product create(String name, Long price, Integer stockQuantity) {
+        return Product.builder()
+                .id(null)
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .build();
+    }
 
     public boolean isAvailable(int requestStockQuantity) {
         return this.stockQuantity > requestStockQuantity;
--- src/test/java/kr/hhplus/be/server/domain/product/domain/model/ProductTest.java
+++ src/test/java/kr/hhplus/be/server/domain/product/domain/model/ProductTest.java
@@ -13,7 +13,7 @@ class ProductTest {
     @DisplayName("상품 재고가 요청 수량보다 많으면 isAvailable은 true를 반환한다")
     void 재고_충분() {
         // given
-        Product product = new Product(1L, "테스트상품", 1000L, 10);
+        Product product = Product.of(1L, "테스트상품", 1000L, 10);
 
         // when
         boolean available = product.isAvailable(5);
@@ -25,7 +25,7 @@ class ProductTest {
     @Test
     @DisplayName("상품 재고가 요청 수량과 같거나 적으면 isAvailable은 false를 반환한다")
     void 재고_부족() {
-        Product product = new Product(1L, "테스트상품", 1000L, 5);
+        Product product = Product.of(1L, "테스트상품", 1000L, 5);
 
         assertFalse(product.isAvailable(5)); // 같을 때
         assertFalse(product.isAvailable(6)); // 초과 요청
@@ -35,7 +35,7 @@ class ProductTest {
     @DisplayName("재고 차감이 정상적으로 이루어진다")
     void 재고차감_정상() {
         // given
-        Product product = new Product(1L, "상품", 2000L, 10);
+        Product product = Product.of(1L, "상품", 2000L, 10);
 
         // when
         product.deductStock(3);
@@ -48,7 +48,7 @@ class ProductTest {
     @DisplayName("재고 차감 후 재고는 음수가 될 수 없다")
     void 재고차감_음수예외() {
         // given
-        Product product = new Product(1L, "상품", 2000L, 2);
+        Product product = Product.of(1L, "상품", 2000L, 2);
 
         // when & then
         assertThrows(ApiException.class, () -> product.deductStock(5));
