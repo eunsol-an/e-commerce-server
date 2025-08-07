4a193739c6c8257e7cb6065c9b0fcae71423765c
eunsol-an <dev.eunsol@gmail.com>
2025-08-08 07:54:55 +0900

    feat: 쿠폰 발급 및 사용 로직에 락 적용
    - 쿠폰 발급을 위한 정책 조회 시 비관적 락 추가 (findByIdWithPessimisticLock)
    - 쿠폰 조회 시 낙관적 락 추가 (findByIdAndUserIdAndStatusIssued)
    - Coupon 및 CouponPolicy 엔티티에 @Version 필드 추가

--- src/main/java/kr/hhplus/be/server/domain/coupon/domain/repository/CouponRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/domain/repository/CouponRepository.java
@@ -5,8 +5,9 @@ import kr.hhplus.be.server.domain.coupon.domain.model.Coupon;
 import java.util.Optional;
 
 public interface CouponRepository {
-    Optional<Coupon> findByIdAndUserId(Long Id, Long userId);
+    Optional<Coupon> findByIdAndUserIdAndStatusIssued(Long Id, Long userId);
     Optional<Coupon> findByCouponPolicyIdAndUserId(Long couponPolicyId, Long userId);
     Coupon save(Coupon coupon);
     boolean existsByUserIdAndCouponPolicyId(Long userId, Long couponPolicyId);
+    void deleteAllInBatch();
 }
--- src/main/java/kr/hhplus/be/server/domain/coupon/infrastructure/persistence/repository/CouponPolicyJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/infrastructure/persistence/repository/CouponPolicyJpaRepository.java
@@ -1,7 +1,15 @@
 package kr.hhplus.be.server.domain.coupon.infrastructure.persistence.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.domain.coupon.infrastructure.persistence.entity.CouponPolicyJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CouponPolicyJpaRepository extends JpaRepository<CouponPolicyJpaEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM CouponPolicyJpaEntity AS p WHERE p.id = :id")
+    Optional<CouponPolicyJpaEntity> findByIdWithPessimisticLock(Long id);
 }
